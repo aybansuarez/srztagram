79212744ac8ffe0b199f289fbacdefbe16aa210b
Ivan Paolo Suarez <jedsuarez@gmail.com>
2020-10-29 02:22:15 +0800

    no access component

---
+++ src/components/NoAccess/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import LockRoundedIcon from '@material-ui/icons/LockRounded';
+
+import noAccessStyle from './styles';
+
+function Header() {
+  const style = noAccessStyle();
+  const isLoggedIn = useSelector(state => state.isLoggedIn);
+
+  return (
+    <Container className={style.root}>
+      <Avatar className={style.avatar}>
+        <LockRoundedIcon />
+      </Avatar>
+      <Typography>This account is private.</Typography>
+      {isLoggedIn ? <Typography>Follow to see the account's posts.</Typography>
+        :
+        <Typography>
+          <Link to="/">Login</Link> to see the account's posts.
+        </Typography>
+      }
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/components/NoAccess/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const noAccessStyle = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginTop: theme.spacing(10),
+    textAlign: 'center'
+  },
+  avatar: {
+    margin: 'auto',
+    marginBottom: theme.spacing(1)
+  },
+
+}));
+
+export default noAccessStyle;
