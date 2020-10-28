9184e6da86b04fd1be78cb0542d027840dba08a2
Ivan Paolo Suarez <jedsuarez@gmail.com>
2020-10-28 18:45:12 +0800

    update users page

--- src/views/UsersList.js
+++ src/views/UsersList.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
 
 import Spinner from '../components/Spinner';
 import UserCard from '../components/UserCard';
@@ -9,33 +8,43 @@ import Main from './Main';
 import backendURL from '../utils/constants';
 
 function Users() {
-  const currentUser = useSelector(state => state.currentUser);
-
-  const url = `${backendURL}/api/profiles/all/except/${currentUser.profile}`;
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  const url = `${backendURL}/api/profiles/search?q=${searchTerm}`;
   let content = null;
 
   const [usersList, setUsersList] = useState({
-    loading: false, data: null, error: false
+    loading: false, data: [], error: false
   });
 
   useEffect(() => {
-    setUsersList({ loading: true, data: null, error: false })
     axios.get(url)
-      .then((response) => {
-        setUsersList({ loading: false, data: response.data, error: false });
+      .then((res) => {
+        setUsersList({ loading: false, data: res.data, error: false })
       })
-      .catch(() => setUsersList({ loading: false, data: null, error: true }))
+      .catch((err) => {
+        console.log(err)
+        setUsersList({ loading: false, data: [], error: true })
+      })
   }, [url])
 
   if (usersList.loading) content = <Spinner whole />;
   if (usersList.error) content = <Error404 />;
 
-  if (usersList.data) {
-    content =
-      usersList.data.map((user, key) =>
+  content =
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      {usersList.data.map((user, key) =>
         <UserCard key={key} user={user} />
-      )
-  }
+      )}
+    </div>
 
   return (
     <Main component={content} />
