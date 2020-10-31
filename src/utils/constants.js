7cd9420ff94ac4e6bf691482aa26f3a524221e6f
Ivan Paolo Suarez <jedsuarez@gmail.com>
2020-10-31 23:56:12 +0800

    add messages api constsnt url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,3 +8,4 @@ export const AUTH_API_URL = `${BACKEND_URL}/api/auth`;
 export const PROFILE_API_URL = `${BACKEND_URL}/api/profiles`;
 export const POSTS_API_URL = `${BACKEND_URL}/api/posts`;
 export const COMMENTS_API_URL = `${BACKEND_URL}/api/comments`;
+export const MESSAGES_API_URL = `${BACKEND_URL}/api/messages`;
