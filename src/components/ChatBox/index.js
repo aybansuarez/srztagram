87f68428c0acd18fdba2fc55184fc202eb1420ab
Ivan Paolo Suarez <jedsuarez@gmail.com>
2020-10-31 23:55:56 +0800

    transfer chatbox

---
+++ src/components/ChatBox/index.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import io from 'socket.io-client';
+import axios from 'axios';
+
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import InputBase from '@material-ui/core/InputBase';
+import Paper from '@material-ui/core/Paper';
+
+import { BACKEND_URL, MESSAGES_API_URL } from '../../utils/constants'
+import defaultLogo from '../../assets/default_avatar.png';
+import chatboxStyle from './styles';
+let socket;
+
+function Chatbox({ chat, profile }) {
+  const style = chatboxStyle();
+  const url = `${MESSAGES_API_URL}/chat/${chat}`
+  const profileID = useSelector(state => state.currentUser.profile);
+
+  const isRendered = useRef(false);
+  const messagesEndRef = useRef('el')
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [profiles, setProfiles] = useState(null);
+
+  const scrollToBottom = () => {
+    if (messages.length > 0) {
+      messagesEndRef.current.scrollIntoView()
+    }
+  }
+
+  useEffect(() => {
+    socket = io(BACKEND_URL);
+    isRendered.current = true;
+    axios.get(url)
+      .then((res) => {
+        if (isRendered.current) {
+          setProfiles(res.data.profiles);
+          setMessages(res.data.messages);
+          socket.emit('join', { profile: profile._id, chat }, () => null);
+        }
+      })
+      .catch((err) => console.log(err));
+
+    return () => {
+      isRendered.current = false;
+      socket.emit('forceDisconnect');
+      socket.off();
+    }
+  }, [url, chat, profile._id]);
+
+  useEffect(() => {
+    isRendered.current = true;
+    socket.on('message', (message) => {
+      if (isRendered.current) setMessages([...messages, message]);
+    });
+
+    return () => isRendered.current = false;
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      const data = { message, profile: profile._id };
+      axios.post(url, data, { withCredentials: true })
+        .then(() => {
+          socket.emit(
+            'sendMessage', { message, profile }, () => setMessage('')
+          );
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  const findProfile = (arr, current) => {
+    return arr.find((profile) => {
+      return profile._id === current;
+    });
+  }
+
+  return (
+    <>
+      {profiles && findProfile(profiles, profileID) ?
+        <>
+          {profiles.map((profile, key) =>
+            <Grid key={key}>
+              {profile._id !== profileID &&
+                <Grid className={style.header}>
+                  {profile.name} ({profile.username})
+                </Grid>
+              }
+            </Grid>
+          )}
+          <Grid className={style.chatbox}>
+            {messages.length && messages.map((message, key) =>
+              <Grid key={key}>
+                {message.profile._id === profile._id ?
+                  <Grid container className={style.chatOne}>
+                    <Grid item xs={7} sm={6} className={style.chatMessageOne}>
+                      <span className={`${style.message} ${style.messageOne}`}>{message.message}</span>
+                    </Grid>
+                    <Grid item xs={2} sm={1} className={style.chatAvatar}>
+                      <Avatar
+                        alt={message.profile.username}
+                        src={message.profile.avatar ? message.profile.avatar : defaultLogo}
+                      />
+                    </Grid>
+                  </Grid>
+                  :
+                  <Grid container className={style.chatTwo}>
+                    <Grid item xs={2} sm={1} className={style.chatAvatar}>
+                      <Avatar
+                        alt={message.profile.username}
+                        src={message.profile.avatar ? message.profile.avatar : defaultLogo}
+                      />
+                    </Grid>
+                    <Grid item xs={7} sm={6} className={style.chatMessageTwo}>
+                      <span className={`${style.message} ${style.messageTwo}`}>{message.message}</span>
+                    </Grid>
+                  </Grid>
+                }
+              </Grid>
+            )}
+            <Grid ref={(el) => messagesEndRef.current = el}></Grid>
+          </Grid>
+          <Grid>
+            <Paper component='form' className={style.chatForm}>
+              <InputBase
+                type='text'
+                value={message}
+                placeholder='Add Message'
+                fullWidth
+                className={style.input}
+                onChange={(e) => {
+                  setMessage(e.target.value); scrollToBottom();
+                }}
+                onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null}
+              />
+              <IconButton
+                type='submit'
+                onClick={(e) => sendMessage(e)}
+                className={style.button}
+              >
+                <SendRoundedIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+        </>
+        : null
+      }
+    </ >
+  )
+}
+
+export default Chatbox;
---
+++ src/components/ChatBox/styles.js
@@ -0,0 +1,89 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const topbarStyle = makeStyles((theme) => ({
+  header: {
+    padding: '10px',
+    backgroundColor: '#15202b',
+    color: '#fff',
+    fontWeight: 'bold',
+    borderBottom: '5px solid #666',
+    borderTop: '5px solid #666',
+  },
+  chatbox: {
+    maxWidth: '684px',
+    maxHeight: 'calc(100vh - 255px)',
+    padding: theme.spacing(.5, 0),
+    overflowY: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      maxHeight: 'calc(100vh - 245px)',
+    },
+  },
+  chatOne: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    margin: '2px 0',
+  },
+  chatTwo: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    margin: '2px 0',
+  },
+  chatAvatar: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  chatMessageOne: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: theme.spacing(1)
+    },
+    [theme.breakpoints.up('md')]: {
+      marginRight: theme.spacing(0)
+    },
+  },
+  chatMessageTwo: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  message: {
+    padding: theme.spacing(1),
+    color: '#fff',
+    borderRadius: '10px',
+    wordBreak: 'break-word',
+    minHeight: '40px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  messageOne: {
+    backgroundColor: '#223344',
+  },
+  messageTwo: {
+    backgroundColor: '#666',
+  },
+  chatForm: {
+    display: 'flex',
+    height: '45px',
+    borderRadius: 0,
+  },
+  input: {
+    paddingLeft: '12px',
+    color: '#183881',
+  },
+  button: {
+    borderRadius: '0',
+    color: '#183881',
+    border: 'none',
+    '&:focus': {
+      outline: 'none'
+    },
+  },
+
+}));
+
+export default topbarStyle;
